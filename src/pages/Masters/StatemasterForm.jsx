7e91812cea432f94fc34a158a913fe53e6b22216
CJCOOL2140 <39669542+CJCOOL2140@users.noreply.github.com>
2024-08-29 19:35:48 +0530

    Update StatemasterForm.jsx

--- src/pages/Masters/StatemasterForm.jsx
+++ src/pages/Masters/StatemasterForm.jsx
@@ -1,27 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { TextField, Grid, Typography, Paper, Box, Tabs, Tab, Table, TableBody, TableContainer, TableHead, TableRow, Checkbox, FormControlLabel } from '@mui/material';
 import axios from 'axios';
+import { STATEMASTER_URL_ENDPOINT } from "../../utils/url_endpoints";
 import SaveIcon from '@mui/icons-material/Save';
 import CancelIcon from '@mui/icons-material/Cancel';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
-import handleDelete from '../deleteHandler';  // Import the common delete handler
-import { CustomButton, StyledTableCell, StyledTableRow } from '../styledComponents';  // Import the styled components
-
-
-
-
-const STATEMASTER_URL_ENDPOINT = "http://chanakyajha-001-site1.gtempurl.com/api/STATEMASTER"; // Update this with your actual API endpoint
-
-
-
+import handleDelete from '../deleteHandler';
+import { CustomButton, StyledTableCell, StyledTableRow } from '../styledComponents';
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
-
-
-
 
 const StateMasterForm = () => {
   const [formData, setFormData] = useState({
@@ -30,45 +20,57 @@ const StateMasterForm = () => {
     isUnionTerritory: false,
   });
 
-
-
-
   const [status, setStatus] = useState(null);
   const [editstatus, setEditStatus] = useState(null);
   const [validationError, setValidationError] = useState(null);
-  const [validationErrorstate, setValidationErrorstate] = useState(null);
-  const [validationErrorcode, setValidationErrorcode] = useState(null);
-  const [validationErrorstateedit, setValidationErrorstateedit] = useState(null);
-  const [validationErrorcodeedit, setValidationErrorcodeedit] = useState(null);
+  const [validationErrorcheck, setValidationErrorcheck] = useState(null);
+  const [validationErrorcheckedit, setValidationErrorcheckedit] = useState(null);
   const [tabValue, setTabValue] = useState(0);
   const [tabs, setTabs] = useState([{ label: 'State Master Details' }]);
   const [stateMasters, setStateMasters] = useState([]);
   const [editingRows, setEditingRows] = useState({});
 
+  const refresherror = () => {
+    setValidationErrorcheck(null);
+    setValidationErrorcheckedit(null);
+  };
 
+  const validationcheckfields = () =>{
+    let errors = {};
+    if (!formData.stateName.trim()) errors.name = 'State Name cannot be blank';
+    if (!formData.stateCode.trim()) errors.code = 'State Code cannot be blank';
+    if (formData.stateName.trim() && formData.stateName.trim().length > 50) errors.len = 'State Name cannot be greater than 50';
+    if (formData.stateCode.trim() && formData.stateCode.trim().length > 2) errors.codelen = 'State Code cannot exceed 2 characters';
+    setValidationErrorcheck(errors);
+    if (Object.keys(errors).length > 0) {
+      return "error";
+    }
+}
 
+const validationcheckfieldsedit = (rowCode) =>{
+  let errors = {};
+  if (!editingRows[rowCode].stateName.trim()) errors.name = 'State Name cannot be blank';
+  if (!editingRows[rowCode].stateCode.trim()) errors.code = 'State Code cannot be blank';
+  if (editingRows[rowCode].stateName.trim() && editingRows[rowCode].stateName.trim().length > 50) errors.len = 'State Name cannot be greater than 50';
+  if (editingRows[rowCode].stateCode.trim() && editingRows[rowCode].stateCode.trim().length > 2) errors.codelen = 'State Code cannot exceed 2 characters';
+  setValidationErrorcheckedit(errors);
+  if (Object.keys(errors).length > 0) {
+    return "error";
+  }
+}
 
   useEffect(() => {
-    if (tabValue === 1) {
-      fetchStateMasters();
-    }
+    if (tabValue === 1) fetchStateMasters();
   }, [tabValue]);
-
-
-
 
   const fetchStateMasters = async () => {
     try {
       const response = await axios.get(`${STATEMASTER_URL_ENDPOINT}`);
-      console.log('Fetched State Masters:', response.data);
       setStateMasters(response.data);
     } catch (error) {
       console.error("Error fetching state masters", error);
     }
   };
-
-
-
 
   const handleChange = (event) => {
     const { name, value, type, checked } = event.target;
@@ -79,16 +81,11 @@ const StateMasterForm = () => {
     setStatus(null);
     setEditStatus(null);
     setValidationError(null);
-    setValidationErrorstate(null);
-    setValidationErrorcode(null);
-    setValidationErrorstateedit(null);
-    setValidationErrorcodeedit(null);
+    refresherror();
   };
 
-
-
-
   const handleEditFormChange = (event, rowCode) => {
+    refresherror();
     const { name, value, type, checked } = event.target;
     setEditingRows({
       ...editingRows,
@@ -97,63 +94,20 @@ const StateMasterForm = () => {
         [name]: type === 'checkbox' ? checked : value,
       },
     });
-    setValidationErrorstate(null);
-    setValidationErrorcode(null);
-    setValidationErrorstateedit(null);
-    setValidationErrorcodeedit(null);
     setEditStatus(null);
   };
 
-
-
-
   const handleSubmit = async (event) => {
     event.preventDefault();
-
-
-    if (!formData.stateName.trim()) {
-      setValidationErrorstate('State Name cannot be blank');
-      return;
-    }
-    if (!formData.stateCode.trim()) {
-      setValidationErrorcode('State Code cannot be blank');
-      return;
-    }
-
-
-
-
-    if (formData.stateName.trim().length > 50) {
-      setValidationErrorstate('State Name cannot exceed 50 characters');
-      return;
-    }
-
-
-
-
-    if (formData.stateCode.trim().length > 2) {
-      setValidationErrorcode('State Code cannot exceed 2 characters');
-      return;
-    }
-
-
-
-
+    if(validationcheckfields() === "error")
+      { return; }
     const isUnionTerritoryValue = formData.isUnionTerritory ? 't' : 'f';
-
-
-
-
     const newState = {
       NAME: formData.stateName.trim(),
       STATECOD: formData.stateCode.trim(),
       ISUT: isUnionTerritoryValue,
-      COUNTRYCODE: '000001' // Assuming this is static or predefined
+      COUNTRYCODE: '000001'
     };
-
-
-
-
     try {
       const response = await axios.post(`${STATEMASTER_URL_ENDPOINT}`, newState, {
         headers: {
@@ -161,30 +115,17 @@ const StateMasterForm = () => {
           'Content-Type': 'application/json'
         }
       });
-
-
-
-
       if (response.status === 201) {
         setStatus({ type: 'success', message: 'State Master added successfully!' });
-        await fetchStateMasters();
         setFormData({ stateName: '', stateCode: '', isUnionTerritory: false });
         await sleep(1000);
         setStatus(null);
         setEditStatus(null);
         setValidationError(null);
-        setValidationErrorstate(null);
-        setValidationErrorcode(null);
-        setValidationErrorstateedit(null);
-         setValidationErrorcodeedit(null); // Clear any previous validation errors
+        refresherror();
       }
-
-
-
-
     } catch (error) {
       if (error.response && error.response.status === 400) {
-        // Check if the error message matches the one from the backend
         if (error.response.data === "State Code already in use.") {
           setValidationError('State Code already in use.');
         } else {
@@ -196,9 +137,6 @@ const StateMasterForm = () => {
     }
   };
 
-
-
-
   const handleEditClick = (state) => {
     const newEditingRows = { ...editingRows };
     if (!newEditingRows[state.code]) {
@@ -209,9 +147,7 @@ const StateMasterForm = () => {
       };
     }
     setEditingRows(newEditingRows);
-    setValidationErrorstateedit(null);
-    setValidationErrorcodeedit(null);
-
+    refresherror();
 
     const editTabExists = tabs.some(tab => tab.label === `Edit ${state.name}`);
     if (!editTabExists) {
@@ -221,26 +157,9 @@ const StateMasterForm = () => {
     setTabValue(editTabIndex >= 0 ? editTabIndex : tabs.length);
   };
 
-
-
-
   const handleUpdateClick = async (rowCode) => {
-    if (!editingRows[rowCode].stateName.trim()) {
-      setValidationErrorstateedit('State Name cannot be blank');
-      return;
-    }
-
-
-
-
-    if (!editingRows[rowCode].stateCode.trim()) {
-      setValidationErrorcodeedit('State Code cannot be blank');
-      return;
-    }
-
-
-
-
+    if(validationcheckfieldsedit(rowCode) === "error")
+      { return; }
     const editedState = {
       CODE: rowCode,
       NAME: editingRows[rowCode].stateName.trim(),
@@ -249,9 +168,6 @@ const StateMasterForm = () => {
       COUNTRYCODE: '000001'
     };
 
-
-
-
     try {
       const response = await axios.put(`${STATEMASTER_URL_ENDPOINT}/${editedState.CODE}`, editedState, {
         headers: {
@@ -259,24 +175,15 @@ const StateMasterForm = () => {
           'Content-Type': 'application/json'
         }
       });
-
-
-
-
       if (response.status === 204) {
         await fetchStateMasters();
         handleTabClose(rowCode);
-        setValidationError(null); // Clear any previous validation errors
+        setValidationError(null);
       }
-
-
-
-
     } catch (error) {
       if (error.response && error.response.status === 400) {
         if (error.response.data === "State Code already in existing record.") {
-          setEditStatus({type: 'error',message: 'State Code already in existing record.'});
-         
+          setEditStatus({ type: 'error', message: 'State Code already in existing record.' });
         } else {
           setValidationError('An error occurred. Please try again.');
         }
@@ -284,23 +191,13 @@ const StateMasterForm = () => {
         setValidationError('An error occurred. Please try again.');
       }
     }
-
-
-   
   };
-
-
-
 
   const handleCancelUpdateClick = (rowCode) => {
     handleTabClose(rowCode);
     setEditStatus(null);
-    setValidationErrorstateedit(null);
-    setValidationErrorcodeedit(null);
+    refresherror();
   };
-
-
-
 
   const handleTabClose = (rowCode) => {
     const tabIndex = tabs.findIndex(tab => tab.code === rowCode);
@@ -309,24 +206,16 @@ const StateMasterForm = () => {
       const { [rowCode]: _, ...remainingRows } = prevRows;
       return remainingRows;
     });
-    setTabValue(1); // Go back to the list tab
-   
+    setTabValue(1);
   };
-
-
-
 
   const handleCancel = () => {
     setFormData({ stateName: '', stateCode: '', isUnionTerritory: false });
     setStatus(null);
     setEditStatus(null);
     setValidationError(null);
-    setValidationErrorstate(null);
-    setValidationErrorcode(null);
+    refresherror();
   };
-
-
-
 
   const handleEdit = () => {
     const listTabExists = tabs.some(tab => tab.label === 'State Master List');
@@ -335,19 +224,12 @@ const StateMasterForm = () => {
     }
     const listTabIndex = tabs.findIndex(tab => tab.label === 'State Master List');
     setTabValue(listTabIndex >= 0 ? listTabIndex : tabs.length);
-    setValidationErrorstate(null);
-    setValidationErrorcode(null);
+    refresherror();
   };
-
-
-
 
   const handleTabChange = (event, newValue) => {
     setTabValue(newValue);
   };
-
-
-
 
   return (
     <div style={{ padding: 1 }}>
@@ -357,16 +239,11 @@ const StateMasterForm = () => {
         ))}
       </Tabs>
 
-
-
-
       <Box p={2}>
         {tabValue === 0 && (
           <form onSubmit={handleSubmit}>
             <Paper style={{ padding: 16, marginBottom: 20 }}>
-              <Typography variant="subtitle1" gutterBottom>
-                State Master Details
-              </Typography>
+              <Typography variant="subtitle1" gutterBottom>State Master Details</Typography>
               <Grid item xs={12} sm={6}>
                 <TextField
                   fullWidth
@@ -376,8 +253,8 @@ const StateMasterForm = () => {
                   onChange={handleChange}
                   variant="outlined"
                   margin="dense"
-                  error={!!validationErrorstate}
-                  helperText={validationErrorstate}
+                  error={!!validationErrorcheck?.name || !!validationErrorcheck?.len}
+                  helperText={validationErrorcheck?.name || validationErrorcheck?.len}
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
@@ -389,9 +266,9 @@ const StateMasterForm = () => {
                   onChange={handleChange}
                   variant="outlined"
                   margin="dense"
-                  inputProps={{ maxLength: 2 }} // Limit input to 2 characters
-                  error={!!validationErrorcode}
-                  helperText={validationErrorcode}
+                  inputProps={{ maxLength: 2 }}
+                  error={!!validationErrorcheck?.code || !!validationErrorcheck?.codelen}
+                  helperText={validationErrorcheck?.code || validationErrorcheck?.codelen}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -407,16 +284,11 @@ const StateMasterForm = () => {
                   label="Is it Union Territory?"
                 />
               </Grid>
-              {/* Add the error message display here */}
               {validationError && (
                 <Typography variant="body2" color="red" style={{ marginTop: 10 }}>
                   {validationError}
                 </Typography>
               )}
-
-
-
-
               {status && (
                 <Typography variant="body2" color={status.type === 'success' ? 'green' : 'red'} style={{ marginTop: 10 }}>
                   {status.message}
@@ -460,14 +332,9 @@ const StateMasterForm = () => {
           </form>
         )}
 
-
-
-
         {tabValue === 1 && (
           <Paper style={{ padding: 16, marginBottom: 1 }}>
-            <Typography variant="subtitle1" gutterBottom>
-              State Master List
-            </Typography>
+            <Typography variant="subtitle1" gutterBottom>State Master List</Typography>
             <TableContainer component={Paper} sx={{ maxHeight: 4000, overflowY: 'auto', borderRadius: '8px', boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.1)' }}>
               <Table stickyHeader sx={{ minWidth: 650, width: '100%' }}>
                 <TableHead>
@@ -518,15 +385,10 @@ const StateMasterForm = () => {
           </Paper>
         )}
 
-
-
-
         {tabValue > 1 && tabs[tabValue] && (
           <form>
             <Paper style={{ padding: 16, marginBottom: 20 }}>
-              <Typography variant="subtitle1" gutterBottom>
-                Edit {editingRows[tabs[tabValue].code]?.stateName}
-              </Typography>
+              <Typography variant="subtitle1" gutterBottom>Edit Record</Typography>
               <Grid item xs={12} sm={6}>
                 <TextField
                   fullWidth
@@ -536,23 +398,15 @@ const StateMasterForm = () => {
                   onChange={(event) => handleEditFormChange(event, tabs[tabValue].code)}
                   variant="outlined"
                   margin="dense"
-                  error={!!validationErrorstateedit}
-                  helperText={validationErrorstateedit}
+                  error={!!validationErrorcheckedit?.name || !!validationErrorcheckedit?.len}
+                  helperText={validationErrorcheckedit?.name || validationErrorcheckedit?.len}
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
-                <TextField
-                  fullWidth
-                  label="State Code"
-                  name="stateCode"
-                  value={editingRows[tabs[tabValue].code]?.stateCode || ''}
-                  onChange={(event) => handleEditFormChange(event, tabs[tabValue].code)}
-                  variant="outlined"
-                  margin="dense"
-                  inputProps={{ maxLength: 2 }} // Limit input to 2 characters
-                  error={!!validationErrorcodeedit}
-                  helperText={validationErrorcodeedit}
-                />
+                <TextField fullWidth label="State Code" name="stateCode" value={editingRows[tabs[tabValue].code]?.stateCode || ''} onChange={(event) => handleEditFormChange(event, tabs[tabValue].code)} variant="outlined" margin="dense" 
+                inputProps={{ maxLength: 2 }} 
+                error={!!validationErrorcheckedit?.code || !!validationErrorcheckedit?.codelen} 
+                helperText={validationErrorcheckedit?.code || validationErrorcheckedit?.codelen}/>
               </Grid>
               <Grid item xs={12}>
                 <FormControlLabel
@@ -567,15 +421,12 @@ const StateMasterForm = () => {
                   label="Is it Union Territory?"
                 />
               </Grid>
- {editstatus && (
+              {editstatus && (
                 <Typography variant="body2" color={editstatus.type === 'success' ? 'green' : 'red'} style={{ marginTop: 10 }}>
                   {editstatus.message}
                 </Typography>
               )}
-
-
             </Paper>
-           
             <Box sx={{ display: 'flex', justifyContent: 'left' }}>
               <Grid container spacing={2} justifyContent="left">
                 <Grid item>
@@ -602,27 +453,9 @@ const StateMasterForm = () => {
             </Box>
           </form>
         )}
-
-
-
-
       </Box>
     </div>
   );
 };
 
-
-
-
 export default StateMasterForm;
-
-
-
-
-
-
-
-
-
-
-
